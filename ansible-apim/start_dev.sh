5e4b7a2ce9b46d1ba0802e8529028f4805a273a9
Asger <asger@redpill-linpro.com>
2025-01-28 18:27:50 +0100

    changes

--- ansible-apim/start_dev.sh
+++ ansible-apim/start_dev.sh
@@ -1,5 +1,7 @@
-#gnome-terminal -- /bin/sh -c 'docker exec apim42 "/bin/sh" "/data/apim-control-plane/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=control-plane -DenableCorrelationLogs=true'
-gnome-terminal -- /bin/sh -c 'docker exec apim42 "/bin/sh" "/data/apim-control-plane/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=control-plane'
+gnome-terminal -- /bin/sh -c 'docker exec $WSO2AM_NAME-apim01 "/bin/sh" "/data/apim/wso2am-4.3.0/bin/api-manager.sh"'
+gnome-terminal -- /bin/sh -c 'docker exec $WSO2AM_NAME-apim02 "/bin/sh" "/data/apim/wso2am-4.3.0/bin/api-manager.sh"'
 sleep 30
-#gnome-terminal -- /bin/sh -c 'docker exec gateway42 "/bin/sh" "/data/apim-gateway/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=gateway-worker -DenableCorrelationLogs=true'
-gnome-terminal -- /bin/sh -c 'docker exec gateway42 "/bin/sh" "/data/apim-gateway/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=gateway-worker'
+gnome-terminal -- /bin/sh -c 'docker exec $WSO2AM_NAME-gateway01 "/bin/sh" "/data/apim-gateway/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=gateway-worker'
+gnome-terminal -- /bin/sh -c 'docker exec $WSO2AM_NAME-gateway02 "/bin/sh" "/data/apim-gateway/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=gateway-worker'
+
+#gnome-terminal -- /bin/sh -c 'docker exec apim42 "/bin/sh" "/data/apim-control-plane/wso2am-4.3.0/bin/api-manager.sh" -Dprofile=control-plane -stop'
